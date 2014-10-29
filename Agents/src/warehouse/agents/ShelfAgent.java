60e7cbf47975e7afecbbdb8c7de1fde6748a4b9c
MBasti <bastian.mager@smail.inf.h-brs.de>
2014-10-29 10:38:52 +0100

    project setting

---
+++ Agents/src/warehouse/agents/ShelfAgent.java
@@ -0,0 +1,16 @@
+package warehouse.agents;
+
+/**
+ * @author Bastian Mager <bastian.mager.2010w@informatik.h-brs.de>
+ */
+public class ShelfAgent {
+
+	private String item;
+	
+	public ShelfAgent(String item) {
+		this.item = item;
+	}
+
+	
+	
+}
