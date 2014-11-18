b7cb709e646178a6ab97f773821283c2d648a2bb
autarch princeps <autarch@outlook.com>
2014-11-18 12:12:18 +0100

    Shebang

--- COMPILE_CLASSES.sh
+++ COMPILE_CLASSES.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 javac -d Agents/bin -classpath Agents/lib/jade.jar Agents/src/*/*/*.java
 read -n1 -r -p "Press any key to continue..."
--- RUN_WAREHOUSE.sh
+++ RUN_WAREHOUSE.sh
@@ -1,1 +1,1 @@
-java -classpath "Agents/lib/jade.jar" jade.Boot -gui -agents warehouse:warehouse.agents.WarehouseAgent;picker:warehouse.agents.OrderPicker;robot_1:warehouse.agents.RobotAgent;shelf_1:warehouse.agents.ShelfAgent"(ROTOR,5)";shelf_2:warehouse.agents.ShelfAgent"(STICK,10)"
+#!/bin/bash	java -classpath "Agents/lib/jade.jar" jade.Boot -gui -agents warehouse:warehouse.agents.WarehouseAgent;picker:warehouse.agents.OrderPicker;robot_1:warehouse.agents.RobotAgent;shelf_1:warehouse.agents.ShelfAgent"(ROTOR,5)";shelf_2:warehouse.agents.ShelfAgent"(STICK,10)"
