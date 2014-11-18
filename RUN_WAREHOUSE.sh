2b44984d113d67f1f3433e9c9d9f1d6a8e38a0d6
autarch princeps <autarch@outlook.com>
2014-11-18 12:39:23 +0100

    FIX Launch

--- RUN_WAREHOUSE.sh
+++ RUN_WAREHOUSE.sh
@@ -1,1 +1,3 @@
-#!/bin/bash	java -classpath "Agents/lib/jade.jar" jade.Boot -gui -agents warehouse:warehouse.agents.WarehouseAgent;picker:warehouse.agents.OrderPicker;robot_1:warehouse.agents.RobotAgent;shelf_1:warehouse.agents.ShelfAgent"(ROTOR,5)";shelf_2:warehouse.agents.ShelfAgent"(STICK,10)"
+#!/bin/bash
+
+java -classpath "Agents/bin:Agents/lib/jade.jar:Agents/lib/common-codec-1.3.jar" jade.Boot -gui -agents warehouse:warehouse.agents.WarehouseAgent\;picker:warehouse.agents.OrderPicker\;robot_1:warehouse.agents.RobotAgent\;shelf_1:warehouse.agents.ShelfAgent"(ROTOR,5)"\;shelf_2:warehouse.agents.ShelfAgent"(STICK,10)"
